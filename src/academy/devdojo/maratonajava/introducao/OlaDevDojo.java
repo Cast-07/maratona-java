a935471299680622e9b278a520d586d61ebfa5bd
adrian-xxadn <Adrian221100@gmail.com>
2025-02-18 16:08:46 -0300

    initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Scanner;
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-KA-ME-HA");
+    }
+}
