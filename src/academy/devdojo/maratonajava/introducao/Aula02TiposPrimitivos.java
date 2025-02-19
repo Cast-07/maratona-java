9ea5e87bb6e6dfa62d6a91dd7fc632ca5227d4b5
adrian-xxadn <Adrian221100@gmail.com>
2025-02-19 15:32:02 -0300

    tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,7 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
+import java.util.Scanner;
+
 public class Aula02TiposPrimitivos {
     public static void main (String[] args){
 
+        int age = 10;
+
+        System.out.println(age);
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Sua idade e "+age+" anos");
     }
 }
