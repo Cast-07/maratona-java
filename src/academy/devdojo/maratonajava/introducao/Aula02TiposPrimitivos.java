a42ccf6fd46674821bf24552eb86513f7c13527c
adrian-xxadn <Adrian221100@gmail.com>
2025-02-19 17:57:27 -0300

    tipos primitivos pt 02

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 
 public class Aula02TiposPrimitivos {
     public static void main (String[] args){
-
+     // int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 10;
 
-        System.out.println(age);
-        Scanner leia = new Scanner(System.in);
         System.out.println("Sua idade e "+age+" anos");
+        System.out.println(falso);
+        System.out.println("char " +caractere);
     }
 }
