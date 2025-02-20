c6476f5742f7f5a308a5b31185e1f6384b16e684
adrian-xxadn <Adrian221100@gmail.com>
2025-02-20 19:02:15 -0300

    tipos primitivos pt 05 exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Adrian";
+        String endereco = "Vila da folha";
+        int salario = 3500;
+        String data = "14/09/1967";
+
+        System.out.println("Eu " +nome+ " morando no endereço " + endereco+ " confirmo que recebi o salário de " +salario+ " na data " + data);
+
+    }
+
+}
+
