9ec388b0894d1f0eb973fe34ed139a0b8bb99cfe
adrian-xxadn <Adrian221100@gmail.com>
2025-02-21 13:30:18 -0300

    operadores pt 04

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        int numero3 = 55;
+        int resultado = numero3 + numero02 + numero01;
+        System.out.println("O valor é : "+resultado);
+
+
+    }
+}
