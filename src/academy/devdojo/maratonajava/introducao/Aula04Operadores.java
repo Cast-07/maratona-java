8331ec5714a09d686329ff2f2a5d035f1261dbd0
adrian-xxadn <Adrian221100@gmail.com>
2025-02-21 19:32:51 -0300

    operadores pt 02 relacionais

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -10,6 +10,21 @@ public class Aula04Operadores {
         int resultado = numero3 + numero02 + numero01;
         System.out.println("O valor é : "+resultado);
 
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
 
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez  = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+
+        System.out.println("isDezMaiorQueVinte: "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte: "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte: "+isDezIgualVinte);
+        System.out.println("isDezIgualDez: "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez: "+isDezDiferenteDez);
     }
 }
